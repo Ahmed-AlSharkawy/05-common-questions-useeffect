714e5f3fbb29480ea7d66364c2adf3cd27b7b843
Ahmed <you@example.com>
2021-06-13 23:06:42 +0000

    initial commit

---
+++ src/Question.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
+
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false)
+
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button
+          id='btn'
+          className='btn'
+          onClick={() => {
+            setShowInfo(!showInfo)
+          }}
+        >
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      <p>{showInfo && info}</p>
+    </article>
+  )
+}
+
+export default Question
